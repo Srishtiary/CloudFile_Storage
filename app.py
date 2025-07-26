2e6b3e7f04e592326d8531e4ca0347226ad0a2c5
Shrishti Arya <115339856+Srishtiary@users.noreply.github.com>
2025-07-26 10:51:33 +0530

    Removed hardcoded secret; now using environment variable

--- app.py
+++ app.py
@@ -64,5 +64,7 @@ def upload_file():
         return jsonify({"error": f"{type(e).__name__}: {str(e)}"}), 500
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+ if __name__ == "__main__":
+     app.run(host='0.0.0.0', port=10000)
+#if __name__ == "__main__":
+#    app.run(host='127.0.0.1', port=5000, debug=True)
